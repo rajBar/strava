ad4bfab573dfda2f018b417d0390199088e8bec6
Raj Bariah <raj.uni@outlook.com>
2020-10-28 21:30:15 +0000

    Updates

---
+++ precache-manifest.37a57aa26484702218a5a31bbf28a1e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818c1258de1ad697ebd2b20bece6a307",
+    "url": "/strava/index.html"
+  },
+  {
+    "revision": "804381817a4aae2207c7",
+    "url": "/strava/static/css/main.2a222ba0.chunk.css"
+  },
+  {
+    "revision": "07ae8e797185b1d04271",
+    "url": "/strava/static/js/2.1ad6d481.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/strava/static/js/2.1ad6d481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804381817a4aae2207c7",
+    "url": "/strava/static/js/main.ffdea77c.chunk.js"
+  },
+  {
+    "revision": "97776e22f989b91efc99",
+    "url": "/strava/static/js/runtime-main.a7916270.js"
+  }
+]);
