0926bbd7e55d3361533b9d4c3f388867a1dc1138
Gulraj Bariah <raj.uni@outlook.com>
2021-02-01 08:52:49 +0000

    Updates

---
+++ precache-manifest.ce64806f464e61d22e751536b923afa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d6de837cf98cf322df70a440bc1aef",
+    "url": "/strava/index.html"
+  },
+  {
+    "revision": "8a4f9620920da8d294ff",
+    "url": "/strava/static/css/main.1eb0b8cd.chunk.css"
+  },
+  {
+    "revision": "3022318d5adb48b6a33d",
+    "url": "/strava/static/js/2.1943d3c5.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/strava/static/js/2.1943d3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4f9620920da8d294ff",
+    "url": "/strava/static/js/main.a21568ea.chunk.js"
+  },
+  {
+    "revision": "97776e22f989b91efc99",
+    "url": "/strava/static/js/runtime-main.a7916270.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/strava/precache-manifest.520f0382f1610b1aad8f05735392cef3.js"
+  "/strava/precache-manifest.ce64806f464e61d22e751536b923afa8.js"
 );
 
 self.addEventListener('message', (event) => {
