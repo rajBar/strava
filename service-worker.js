f1163107ad412abdf5e355e07db22ebcc830ccc9
Gulraj Bariah <raj.uni@outlook.com>
2021-02-22 13:13:59 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/strava/precache-manifest.98eacbd22e96468bc57ccf703ba387c1.js"
+  "/strava/precache-manifest.8eeb705abc111840cf9ab8e2759637a8.js"
 );
 
 self.addEventListener('message', (event) => {
