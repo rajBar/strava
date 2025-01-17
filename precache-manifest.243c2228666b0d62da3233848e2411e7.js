a3e0d650cd9637f87686daac1030f367926423df
Gulraj Bariah <raj.uni@outlook.com>
2025-01-17 12:10:53 +0000

    Updates

---
+++ precache-manifest.243c2228666b0d62da3233848e2411e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3aa6870681cd11b3c9d17e2a263ad6a",
+    "url": "/strava/index.html"
+  },
+  {
+    "revision": "6da976a058fd0f9782b6",
+    "url": "/strava/static/css/main.ad5db740.chunk.css"
+  },
+  {
+    "revision": "699d5d894edff240a6d6",
+    "url": "/strava/static/js/2.c5ec4460.chunk.js"
+  },
+  {
+    "revision": "8da7310d3b6f76323269112944284304",
+    "url": "/strava/static/js/2.c5ec4460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da976a058fd0f9782b6",
+    "url": "/strava/static/js/main.a9edb6b8.chunk.js"
+  },
+  {
+    "revision": "97776e22f989b91efc99",
+    "url": "/strava/static/js/runtime-main.a7916270.js"
+  }
+]);
