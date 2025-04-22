9806e3681bfa23e32aba0f8f576a8afaf802d2bc
Raj Bariah <rajbariah@gmail.com>
2025-04-22 15:42:53 +0100

    Updates

---
+++ precache-manifest.e94394ca6c24be94cb7a213727af6276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f619c26a77bdd8bc87ed693b54de4e71",
+    "url": "/strava/index.html"
+  },
+  {
+    "revision": "947dcee738cec6508f5b",
+    "url": "/strava/static/css/main.c6b54710.chunk.css"
+  },
+  {
+    "revision": "d9beb449a429943d39a0",
+    "url": "/strava/static/js/2.d0071c05.chunk.js"
+  },
+  {
+    "revision": "8da7310d3b6f76323269112944284304",
+    "url": "/strava/static/js/2.d0071c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947dcee738cec6508f5b",
+    "url": "/strava/static/js/main.ac585f65.chunk.js"
+  },
+  {
+    "revision": "97776e22f989b91efc99",
+    "url": "/strava/static/js/runtime-main.a7916270.js"
+  }
+]);
